90fbfc2d96c71385b6cc7f491f856a7ec5a0393a
gangwarsomya <gangwarsomya16@gmail.com>
2024-06-18 18:40:13 +0530

    cursor

--- script.js
+++ script.js
@@ -792,6 +792,26 @@ function loadPreset() {
 }
 document.getElementById('savePresetButton').addEventListener('click', savePreset);
 document.getElementById('loadPresetButton').addEventListener('click', loadPreset);
+const cursor=document.querySelector(".cursor");
+var timeout;
+document.addEventListener("mousemove",(e)=>{
+    let x = e.pageX;
+    let y =e.pageY;
+
+    cursor.style.top = y+"px";
+    cursor.style.left = x+"px";
+    cursor.style.display = "block";
+
+    function mousestopped(){
+        cursor.style.display = "none";
+    }
+    clearTimeout(timeout);
+    timeout=setTimeout(mousestopped,1000);
+    
+});
+document.addEventListener("mouseout",()=>{
+    cursor.style.display = "none";
+});
 
 
 
