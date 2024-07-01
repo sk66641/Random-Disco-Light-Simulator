e20077c4f78f041ecbfe1a879711f77c7ef1ab15
THENIKHILRAJ <nraj99858@gmail.com>
2024-07-01 13:39:17 +0530

    Add scroll to top button with progress button

--- js/backToTopBtn.js
+++ js/backToTopBtn.js
@@ -1,21 +1,21 @@
 // Get the button
-let backToTopBtn = document.getElementById("backToTopBtn");
+//let backToTopBtn = document.getElementById("backToTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-    scrollFunction();
-};
+//window.onscroll = function() {
+  //  scrollFunction();
+//};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        backToTopBtn.style.display = "block";
-    } else {
-        backToTopBtn.style.display = "none";
-    }
-}
+//function scrollFunction() {
+  //  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      //  backToTopBtn.style.display = "block";
+    //} else {
+       // backToTopBtn.style.display = "none";
+    //}
+//}
 
 // When the user clicks on the button, scroll to the top of the document
-backToTopBtn.addEventListener("click", function() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-});
+//backToTopBtn.addEventListener("click", function() {
+  //  document.body.scrollTop = 0; // For Safari
+    //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+//});
