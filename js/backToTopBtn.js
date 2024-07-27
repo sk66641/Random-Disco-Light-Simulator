a809342b1475ab2f35617c44c784157c34505747
Radhika Malpani <radhikavishnudas2002@gmail.com>
2024-07-27 16:40:08 +0530

    Update backtotopbtn.js

--- js/backToTopBtn.js
+++ js/backToTopBtn.js
@@ -1,21 +1,22 @@
-// Get the button
+
 let backToTopBtn = document.getElementById("backToTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
     scrollFunction();
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         backToTopBtn.style.display = "block";
     } else {
         backToTopBtn.style.display = "none";
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 backToTopBtn.addEventListener("click", function() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
